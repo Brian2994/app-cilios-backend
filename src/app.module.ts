722ac5727172526a7763d091a0b0ee8d799464de
Brian2994 <brianpari29@gmail.com>
2026-04-26 23:13:43 -0300

    feat: registra novos módulos e serviços no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ClientModule } from './client/client.module';
+import { ServiceModule } from './service/service.module';
 import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ClientModule, AppointmentModule],
+  imports: [PrismaModule, AuthModule, ClientModule, ServiceModule, AppointmentModule],
 })
 export class AppModule { }
