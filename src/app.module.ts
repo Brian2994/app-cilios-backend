cf3e82c4726ed313ad293903b5ed8637590e920a
Brian2994 <brianpari29@gmail.com>
2026-04-29 23:58:35 -0300

    chore: registra DashboardModule no AppModule principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { ClientModule } from './client/client.module';
 import { ServiceModule } from './service/service.module';
 import { AppointmentModule } from './appointment/appointment.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ClientModule, ServiceModule, AppointmentModule],
+  imports: [PrismaModule, AuthModule, ClientModule, ServiceModule, AppointmentModule, DashboardModule],
 })
 export class AppModule { }
