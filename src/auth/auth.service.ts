933fa345309fe5f2a64da991fe072237885417df
Brian2994 <brianpari29@gmail.com>
2026-05-04 20:12:37 -0300

    feat(auth-service): implementa lógica de persistência e inclusão do name no JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {
     Injectable,
     UnauthorizedException,
     ConflictException,
+    BadRequestException,
 } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
@@ -14,23 +15,28 @@ export class AuthService {
         private jwtService: JwtService,
     ) { }
 
-    async register(email: string, password: string) {
+    async register(name: string, email: string, password: string) {
+        if (!name || !email || !password) {
+            throw new BadRequestException('Dados obrigatórios');
+        }
+
         const existingUser = await this.prisma.user.findUnique({
             where: { email },
         });
 
         if (existingUser) {
-            throw new ConflictException('Email already in use');
+            throw new ConflictException('Email já cadastrado');
         }
 
         const hash = await bcrypt.hash(password, 10);
 
         const user = await this.prisma.user.create({
-            data: { email, password: hash },
+            data: { name, email, password: hash },
         });
 
         return {
             id: user.id,
+            name: user.name,
             email: user.email,
         };
     }
@@ -54,6 +60,7 @@ export class AuthService {
             access_token: this.jwtService.sign({
                 sub: user.id,
                 email: user.email,
+                name: user.name,
             }),
         };
     }
