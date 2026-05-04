596f09210afdfc21f62d03e33bebceae6e4ffbd6
Brian2994 <brianpari29@gmail.com>
2026-05-04 20:12:19 -0300

    feat(auth-controller): adiciona campo name ao endpoint de registro

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,7 @@ export class AuthController {
 
     @Post('register')
     register(@Body() body: any) {
-        return this.authService.register(body.email, body.password);
+        return this.authService.register(body.name, body.email, body.password);
     }
 
     @Post('login')
